4d43b5536ce39687027e002270b8ed4acd918e02
LucasZubu <lucascaires3641@gmail.com>
2023-05-04 20:26:54 -0300

    Subir Alterações

--- src/app/alterar-cliente/alterar-cliente.page.ts
+++ src/app/alterar-cliente/alterar-cliente.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ClientesService } from '../services/clientes.service';
+import { Cliente } from '../models/Cliente.model';
 
 @Component({
   selector: 'app-alterar-cliente',
@@ -32,7 +33,22 @@ export class AlterarClientePage implements OnInit {
   }
 
   salvar(){
-    
+    if(this.senha == this.confirmeSenha){
+      const cliente : Cliente = {
+        id: this.id,
+        nome: this.nome,
+        email : this.email,
+        senha : this.senha
+      }
+      this.clienteService.update(cliente).subscribe(dados => {
+        alert('Cliente Alterado com sucesso: ' + dados.id)
+        // navegação vem aqui
+        this.route.navigateByUrl('/home');
+        //this.route.navigate(['/home']);
+      })
+      //Nunca colocar a navegação fora... vai voltar sem saber a resposta
+    } else {
+      alert('Senhas não conferem.')
+    }
   }
-
 }
